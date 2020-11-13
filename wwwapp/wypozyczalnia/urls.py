6e433d8c55411d2ae9c4cf8329787bb4bda9a62d
Jacek Ruszkowski <jac.ruszkowski@gmail.com>
2020-11-13 15:48:01 +0100

    wypozyczalnia_ksiazek_4

--- wwwapp/wypozyczalnia/urls.py
+++ wwwapp/wypozyczalnia/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import CategoryList
+# from .views import CategoryList
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('categories/', CategoryList.as_view(), name='categories'),
+    path('category/<str:slug>/', views.category, name='categories'),
 ]
--- wwwapp/wypozyczalnia/views.py
+++ wwwapp/wypozyczalnia/views.py
@@ -1,27 +1,21 @@
 from django.shortcuts import render
 from .models import Book, Category
-from django.views.generic import ListView
-
 
 
 # przy użyciu metody:
 def home(request):
-    content = {'books': Book.objects.all()}
+    categories = Category.objects.all()
+    books = Book.objects.all()
+    content = {'categories': categories,
+               'books': books,
+               }
     return render(request, 'wypozyczalnia/home.html', content)
 
 
-class CategoryList(ListView):
-    model = Category
-    template_name = 'wypozyczalnia/category_view.html'
-    context_object_name = 'categories'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['books'] = Book.objects.all()
-        return  context
-
-# def categories(request):
-#     content = {'categories': Category.objects.all(),
-#                'all_books': Book.objects.all(),
-#                }
-#     return render(request, 'wypozyczalnia/category_view.html', content)
+def category(request, slug):
+    category = Category.objects.get(slug=slug)
+    books = Book.objects.all()
+    content = {'category': category,
+               'books': books,
+               }
+    return render(request, 'wypozyczalnia/category_view.html', content)
